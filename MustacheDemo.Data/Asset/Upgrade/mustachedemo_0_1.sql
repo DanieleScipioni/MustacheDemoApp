5a359a30533c1c77ee6590030b42d8ca9f2fa9da
Daniele Scipioni <daniele.scipioni@gmail.com>
2017-07-22 15:06:39 +0200

    Add default data

--- MustacheDemo.Data/Asset/Upgrade/mustachedemo_0_1.sql
+++ MustacheDemo.Data/Asset/Upgrade/mustachedemo_0_1.sql
@@ -5,3 +5,8 @@
 CREATE TABLE [templates](
     [name] TEXT PRIMARY KEY NOT NULL UNIQUE, 
     [template] TEXT);
+
+INSERT INTO [templates]
+([name],[template])
+VALUES
+('First template' ,'Hello {{Name}}\nYou have just won {{Value}} {{Currency}}!\n{{#InCa}}\nWell, {{TaxedValue}} {{Currency}}, after taxes.\n{{/InCa}}\n{{#List}}\n{{.}}\n{{/List}}\n{{#d}}\n{{e}}: {{tt}}\n{{/d}});');
